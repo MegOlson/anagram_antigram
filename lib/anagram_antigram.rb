edf48c398ffb598b338b4733a3ee643b6e52bf05
mo <meganannetteolson@yahoo.com>
2017-09-08 10:09:42 -0700

    add custom class

--- lib/anagram_antigram.rb
+++ lib/anagram_antigram.rb
@@ -1,18 +1,24 @@
-# class Anagram
-  public def anagram_check(word1, word2)
-    result = word1.size == word2.size && word1.delete(word2).empty?
-
+class Anagram
+  def initialize(word1, word2)
+    @word1 = word1
+    @word2 = word2
+  end
+  def anagram_check
+    @word1 = @word1.downcase
+    @word2 = @word2.downcase
+    result = @word1.size == @word2.size && @word1.delete(@word2).empty?
     if result == true
       return "These words are anagrams"
     else
       return "These words are not anagrams"
     end
+  end
 
+  # public def palindrome_check
   #   word1.each_index do |i|
   #    if word1[i] == word1[word1.length - i -1]
   #      return "palindrome"
   #    else return "not a palindrome"
   #  end
-
-  end
-# end
+  # end
+end
--- spec/anagram_antigram_spec.rb
+++ spec/anagram_antigram_spec.rb
@@ -1,14 +1,17 @@
 require('rspec')
 require('anagram_antigram')
 
-  describe('#anagram_check') do
+  describe('Anagram#anagram_check') do
     it("check if two words are anagrams") do
-      expect(("ruby" "bury").anagram_check("ruby", "bury")).to(eq("These words are anagrams"))
+      anagram = Anagram.new("ruby", "bury")
+      expect(anagram.anagram_check).to(eq("These words are anagrams"))
     end
     it("check if two words are anagrams") do
-      expect(("hello" "hi").anagram_check("hello", "hi")).to(eq("These words are not anagrams"))
+      anagram = Anagram.new("hello", "hi")
+      expect(anagram.anagram_check).to(eq("These words are not anagrams"))
     end
-    # it("check if two words are anagrams despite case") do
-    #   expect("Tea" "Eat").anagram_check("Tea", "Eat")).to.(eq("These words are anagrams"))
-    # end
+    it("check if two words are anagrams despite case") do
+      anagram = Anagram.new("Tea", "Eat")
+      expect(anagram.anagram_check).to(eq("These words are anagrams"))
+    end
   end
