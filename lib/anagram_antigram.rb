853712b9fc464b200ffd812d6e97e2cff60c71b5
mo <meganannetteolson@yahoo.com>
2017-09-08 11:08:19 -0700

    check if anagrams are also palindromes

--- lib/anagram_antigram.rb
+++ lib/anagram_antigram.rb
@@ -9,16 +9,19 @@ class Anagram
     result = @word1.size == @word2.size && @word1.delete(@word2).empty?
     if result == true
       return "These words are anagrams"
+      palindrome_check
     else
       return "These words are not anagrams"
     end
   end
 
-  # public def palindrome_check
-  #   word1.each_index do |i|
-  #    if word1[i] == word1[word1.length - i -1]
-  #      return "palindrome"
-  #    else return "not a palindrome"
-  #  end
-  # end
+  def palindrome_check
+    @word1 = @word1.downcase
+    @word2 = @word2.downcase
+    if @word1 == @word1.reverse || @word2 == @word2.reverse
+      return "These words are anagrams and palindromes"
+    else
+      return "These words are only anagrams"
+    end
+  end
 end
--- spec/anagram_antigram_spec.rb
+++ spec/anagram_antigram_spec.rb
@@ -2,16 +2,24 @@ require('rspec')
 require('anagram_antigram')
 
   describe('Anagram#anagram_check') do
-    it("check if two words are anagrams") do
+    it("checks if two words are anagrams") do
       anagram = Anagram.new("ruby", "bury")
       expect(anagram.anagram_check).to(eq("These words are anagrams"))
     end
-    it("check if two words are anagrams") do
+    it("checks if two words are anagrams") do
       anagram = Anagram.new("hello", "hi")
       expect(anagram.anagram_check).to(eq("These words are not anagrams"))
     end
-    it("check if two words are anagrams despite case") do
+    it("checks if two words are anagrams despite case") do
       anagram = Anagram.new("Tea", "Eat")
       expect(anagram.anagram_check).to(eq("These words are anagrams"))
+    end
+    it("checks if anagrams are also palindromes") do
+      anagram = Anagram.new("Nana", "Anna")
+      expect(anagram.palindrome_check).to(eq("These words are anagrams and palindromes"))
+    end
+    it("checks if anagrams are also palindromes") do
+      anagram = Anagram.new("nap", "pan")
+      expect(anagram.palindrome_check).to(eq("These words are only anagrams"))
     end
   end
